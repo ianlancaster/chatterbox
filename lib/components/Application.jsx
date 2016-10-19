cac1d999d9475e1c767bd3c389edbe63b1eba0d7
Ryan Westlake <ryancwestlake@gmail.com>
2016-10-19 17:49:34 -0600

    broke up application into multiple components

---
+++ lib/components/Application.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { pick, map, extend } from 'lodash'
+import firebase, { reference, signIn } from '../firebase'
+import UserInput from './UserInput.jsx'
+import MessagesContainer from '../containers/MessagesContainer.jsx'
+
+export default class Application extends Component {
+  constructor() {
+    super()
+    this.state = {
+      user: null,
+    }
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => this.setState({ user }))
+  }
+
+  render() {
+    const { user } = this.state
+
+    return (
+      <div className="Application">
+      {/* TODO: move user name to log in status components */}
+        {user ? <p>Hello {user.displayName}</p> : <button onClick={() => signIn()}>Sign In</button> }
+        <MessagesContainer />
+        <UserInput user={user} />
+      </div>
+    )
+  }
+}
--- lib/containers/MessagesContainer.jsx
+++ lib/containers/MessagesContainer.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import { pick, map, extend } from 'lodash'
 import firebase, { reference, signIn } from '../firebase'
-import UserInput from './UserInput.jsx'
+
+const Message = require('../components/Message.jsx')
 
 export default class Application extends Component {
   constructor() {
     super()
     this.state = {
       messages: [],
-      user: null,
     }
   }
 
@@ -19,21 +19,16 @@ export default class Application extends Component {
         messages: map(messages, (val, key) => extend(val, { key })),
       })
     })
-
-    firebase.auth().onAuthStateChanged(user => this.setState({ user }))
   }
 
   render() {
-    const { user, messages } = this.state
+    const { messages } = this.state
 
     return (
-      <div className="Application">
-      {/* TODO: move user name to log in status components */}
-        {user ? <p>Hello {user.displayName}</p> : <button onClick={() => signIn()}>Sign In</button> }
+      <div>
         <ul>
-          { this.state.messages.map(m => <li key={m.key}>{m.user.displayName}: {m.content}</li>) }
+          { messages.map(m => <Message key={m.key} name={m.user.displayName} content={m.content} />) }
         </ul>
-        <UserInput user={user} />
       </div>
     )
   }
