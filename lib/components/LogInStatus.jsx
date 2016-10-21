ba6d40af88f5b3835cf4a84e6f68c9804e2bfde5
Ian Lancaster <ianclancaster@gmail.com>
2016-10-21 01:09:34 -0600

    migrated firebase authentication functionality from app to LogInStatus Component

--- lib/components/LogInStatus.jsx
+++ lib/components/LogInStatus.jsx
@@ -1,22 +1,25 @@
 import React, { Component } from 'react'
-import firebase, { reference, signIn } from '../firebase'
+import firebase, { reference, signIn, signOut } from '../firebase'
+import Button from './Button.jsx'
 
 export default class LogInStatus extends Component {
   constructor(props) {
     super()
+    this.state = {
+      // user: null,
+    }
   }
 
-  // componentDidMount() {
-  //   // firebase.auth().onAuthStateChanged(user => this.setState({ user }))
-  //   firebase.auth().onAuthStateChanged(user => this.props.setUser('ok'))
-  // }
-  // check to see if there is a user
-  // if so change the state and display the name
-  // change the sign in button text to sign out
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => this.props.updateUser(user))
+  }
 
   render() {
     return (
-      this.props.user ? <p>Hello {this.props.user.displayName}</p> : <button onClick={this.props.action}>Sign In</button>
+      <section>
+        <Button text='Sign Out' action={() => signOut() }/>
+        {this.props.user ? <p>Hello {this.props.user.displayName}</p> : <Button text='Sign In' action={() => signIn()}/> }
+      </section>
     )
   }
 }
