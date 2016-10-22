47a01dd807d6897c562a291ee8d890b4333c8ffb
Ian Lancaster <ianclancaster@gmail.com>
2016-10-22 12:03:07 -0600

    added application feature tests

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -1,14 +1,25 @@
 import React from 'react'
 
 import { shallow, mount, render } from 'enzyme'
-import { assert } from 'chai'
+import { expect } from 'chai'
 
 import Application from '../lib/components/Application.jsx'
+import UserInput from '../lib/components/UserInput.jsx'
+import MessagesContainer from '../lib/containers/MessagesContainer.jsx'
+import LogInStatus from '../lib/components/LogInStatus.jsx'
+
 
 describe('Application', () => {
   it('renders as a <div>', () => {
-    const wrapper = shallow(<Application />)
-    assert.equal(wrapper.type(), 'div')
+    const wrapper = mount(<Application />)
+    // expect(wrapper.text()).to.contain('Likes: 0Like! (+1)Dislike! (-1)')
+    expect(wrapper.html()).to.contain('<div class="app">')
+  })
+
+  it('should contain a MessagesContainer object', () => {
+    const wrapper = mount(<Application />)
+    const user = null
+    expect(wrapper.contains(<MessagesContainer user={user} />)).to.be.true
   })
 })
 
