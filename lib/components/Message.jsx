6c71a7565f9936aa86f08ad9bf92a1038ee6c3d7
Ryan Westlake <ryancwestlake@gmail.com>
2016-10-22 14:06:02 -0600

    split name to display first name

--- lib/components/Message.jsx
+++ lib/components/Message.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import moment from 'moment'
 
+
 const Message = (props) => {
+  const name = props.name.split(' ')[0]
   return (
       <li>
-        <p className='message-detail'>{props.time} {props.name} </p>
+        <p className='message-detail'>{props.time} {name} </p>
         <p className='message'>{props.content}</p>
       </li>
   )
