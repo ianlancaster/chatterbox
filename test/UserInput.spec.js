96d2ad08ca586bed9b4e0f0f84934155602eb3ff
Ryan Westlake <ryancwestlake@gmail.com>
2016-10-25 13:51:46 -0600

    changed set up desc and author in package.json, also installed chai-jquery

--- test/UserInput.spec.js
+++ test/UserInput.spec.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import { shallow, mount, render } from 'enzyme'
 import { expect } from 'chai'
-import Application from '../lib/components/Application.jsx'
 import UserInput from '../lib/components/UserInput.jsx'
-import LogInStatus from '../lib/components/LogInStatus.jsx'
 import mockFirebase from './helpers/mockFirebase.js'
 
-require('sinon')
+const sinon = require('sinon')
+require('chai-jquery')
 
 describe('UserInput', () => {
-  it('should display an input field', () => {
+  context('feature tests', () => {
     const wrapper = mount(<UserInput />)
-    expect(wrapper.find('.message-input')).to.have.length(1)
+    it('should display an input field and two buttons', () => {
+      expect(wrapper.find('.message-input')).to.have.length(1)
+      expect(wrapper.find('.add-btn')).to.have.length(1)
+      expect(wrapper.find('.clear-btn')).to.have.length(1)
+    })
+
+    it('should have a character-count with an initial value of 140', () => {
+      expect(wrapper.find('.character-count')).to.have.length(1)
+    })
+
+    it('the character count should decrease when user types in input field', () => {
+      let spy = sinon.spy()
+      wrapper.setState({ draftMessage: 'a' })
+      // expect(wrapper.find('.message-input')).to.have.value('a')
+      expect(wrapper.find('.character-count')).to.have.text('139')
+    })
   })
 })
