3f3be622c7d501d88c0f257dd387078ecea0e3ef
Ryan Westlake <ryancwestlake@gmail.com>
2016-10-25 18:19:53 -0600

    wrote sort tests

--- test/Sort.spec.js
+++ test/Sort.spec.js
@@ -2,23 +2,22 @@ import React from 'react'
 import { shallow, mount, render } from 'enzyme'
 import { expect } from 'chai'
 import mockFirebase from './helpers/mockFirebase.js'
-import MessagesContainer from '../lib/containers/MessagesContainer.jsx'
-import Filter from '../lib/components/Filter.jsx'
+import Sort from '../lib/components/Sort.jsx'
 
 const sinon = require('sinon')
 const locus = require('locus')
 
 
-describe('Filter Container', () => {
+describe('Sort Container', () => {
   context('feature tests', () => {
     it('renders as a section', () => {
-      const wrapper = shallow(<Filter />)
+      const wrapper = shallow(<Sort />)
       expect(wrapper.type()).to.equal('section')
     })
 
-    it('should have an input field', () => {
-      const wrapper = shallow(<Filter />)
-      expect(wrapper.find('.filter-field')).to.have.length(1)
+    it('should have two sort buttons', () => {
+      const wrapper = mount(<Sort />)
+      expect(wrapper.find('.sm-btn')).to.have.length(2)
     })
   })
 })
